1c2fe8bee9722f4c1d8f73951e84b5fa183f666b
@fominivan82 <infomin@yndex.ru>
2019-08-29 18:45:31 +0300

    4задание

--- source/js/script-form.js
+++ source/js/script-form.js
@@ -1,10 +1,10 @@
-var btnmenu = document.querySelector(".main-nav__button");
+var btnMenu = document.querySelector(".main-nav__button");
 var menu = document.querySelector(".main-nav__list");
 
 
-btnmenu.addEventListener("click", function(evt) {
+btnMenu.addEventListener("click", function(evt) {
   evt.preventDefault();
   menu.classList.toggle("main-nav__list--show");
-  btnmenu.classList.toggle("main-nav__button--close");
-  btnmenu.focus();
+  btnMenu.classList.toggle("main-nav__button--close");
+  btnMenu.focus();
 });
--- source/js/script-index.js
+++ source/js/script-index.js
@@ -1,14 +1,14 @@
-var btnmenu = document.querySelector(".main-nav__button");
+var btnMenu = document.querySelector(".main-nav__button");
 var menu = document.querySelector(".main-nav__list");
 var order = document.querySelector(".ware__link");
 var popup = document.querySelector(".cart-modal");
 var close = document.querySelector(".order__overlay");
 
-btnmenu.addEventListener("click", function(evt) {
+btnMenu.addEventListener("click", function(evt) {
   evt.preventDefault();
   menu.classList.toggle("main-nav__list--show");
-  btnmenu.classList.toggle("main-nav__button--close");
-  btnmenu.focus();
+  btnMenu.classList.toggle("main-nav__button--close");
+  btnMenu.focus();
 });
 
 order.addEventListener("click", function(evt) {
